027fdec371ebae657e85a9cbae4290e810530ad5
Matheus Alexandre <matheusalexandre@Matheuss-MacBook-Pro.local>
2018-05-15 11:27:59 -0300

    Creating tests for button and table

---
+++ src/__mocks__/_mock.table.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    _id: Math.random(),
+    name: 'NOME A',
+    unitName: 'unitName A',
+    createdAt: new Date(),
+    images: {
+      ref: 'logo',
+      file: {
+        url: 'https tan tan tan',
+      },
+    },
+  },
+  {
+    _id: Math.random(),
+    name: 'NOME B',
+    unitName: 'unitName B',
+    createdAt: new Date(),
+    images: {
+      ref: 'logo',
+      file: {
+        url: 'https tan tan tan',
+      },
+    },
+  },
+  {
+    _id: Math.random(),
+    name: 'NOME C',
+    unitName: 'unitName C',
+    createdAt: new Date(),
+    images: {
+      ref: 'logo',
+      file: {
+        url: 'https tan tan tan',
+      },
+    },
+  },
+  {
+    _id: Math.random(),
+    name: 'NOME D',
+    unitName: 'unitName D',
+    createdAt: new Date(),
+    images: {
+      ref: 'logo',
+      file: {
+        url: 'https tan tan tan',
+      },
+    },
+  },
+];
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import WrapperTable from './WrapperTable';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path={'/'} exact={true} component={App} />
+      <Route path={'/table'} component={WrapperTable} />
+    </Switch>
+  </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
